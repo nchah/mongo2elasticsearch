297148007c5ed15a9cdcd7e8ef40e5ce8988ec88
Niel C <nielchah@gmail.com>
2017-06-28 23:30:18 -0400

    python

--- python/es-query.py
+++ python/es-query.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python
 
-
+import argparse
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
-response = es.search(index="", doc_type="", body={"query": {}})
 
-print("%d documents found" % response['hits']['total'])
-for doc in response['hits']['hits']:
-    print("%s) %s" % (doc['_id'], doc['_source']['content']))
-
+def get_queries(input_file):
+    """ Get the list of queries from an external file"""
+    return queries
 
 
+def query_es():
+    """ """
+    response = es.search(index="", doc_type="", body={"query": {}})
+
+    print("%d documents found" % response['hits']['total'])
+    for doc in response['hits']['hits']:
+        print("%s) %s" % (doc['_id'], doc['_source']['content']))  # Adjust the JSON fields
+
+    # Set how the query responses should be stored
+
+
+
+if __name__ == '__main__':
+    #parser = argparse.ArgumentParser()
+    #parser.add_argument('markerset_file', help='Path to the input data file')
+    #args = parser.parse_args()
+    #main(args.input_file)
+    query_es()
 
