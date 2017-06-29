e2a4d4463debe720868b86d69af68534d3d19057
Niel C <nielchah@gmail.com>
2017-06-28 23:04:09 -0400

    ES python query script

--- python/es-query.py
+++ python/es-query.py
@@ -6,6 +6,11 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
+response = es.search(index="", doc_type="", body={"query": {}})
+
+print("%d documents found" % response['hits']['total'])
+for doc in response['hits']['hits']:
+    print("%s) %s" % (doc['_id'], doc['_source']['content']))
 
 
 
