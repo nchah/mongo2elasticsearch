a1754cf1e5ab179b84e046b3030a3d650b296a91
Niel C <nielchah@gmail.com>
2017-07-13 21:12:31 -0400

    sed/gsed compatibility

--- scripts/s2-preprocess-mongo-data.sh
+++ scripts/s2-preprocess-mongo-data.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 # Usage:
 # $ bash s2-preprocess-mongo-data.sh [file_to_preprocess]
+# Note: use `gsed` on MacOS. `sed` used here should work fine on Ubuntu systems.
 
 # Add {"index": {}} prior to each JSON line, required for Elasticsearch to index in bulk
-gsed -e 's/^/{ "index" : {} }\n/' -i $1
+sed -e 's/^/{ "index" : {} }\n/' -i $1
 
 # Replace "\n" --> "newline-char"
-gsed -e 's/"\\n"/"newline-char"/g' -i $1
+sed -e 's/"\\n"/"newline-char"/g' -i $1
 
 # Replace {"$oid" --> {"oid"
-gsed -e 's/{"$oid"/{"oid"/g' -i $1
+sed -e 's/{"$oid"/{"oid"/g' -i $1
 
 # Replace {"_id" --> {"mongo_id" - this id is from mongoexport
-gsed -e 's/{"_id"/{"mongo_id"/g' -i $1
+sed -e 's/{"_id"/{"mongo_id"/g' -i $1
 
 # Replace "" field --> "null-val"
-gsed -e 's/{""/{"null-val"/g' -i $1
+sed -e 's/{""/{"null-val"/g' -i $1
 
